138707ec07402113ad6f1647e4541bfbafccee21
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-09 15:40:38 -0500

    Create Home component Set Container from reactstrap Set title Set lorem paragraph from faker

---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import { Container } from 'reactstrap';
+import faker from 'faker';
+
+const Home = () => {
+  return (
+    <Container fluid={ true }>
+      <h4>Home</h4>
+      <p>{ faker.lorem.paragraph() }</p>
+    </Container>
+  );
+}
+
+export default Home;
