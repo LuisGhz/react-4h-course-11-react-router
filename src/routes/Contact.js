5508a2baa43a6b16e6a03a6e3e316db27eee4d25
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-12 15:15:35 -0500

    Set lorem paragraph

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,10 +1,13 @@
+import { Container } from 'react-bootstrap';
+import faker from 'faker';
 
 const Contact = () => {
   return (
-    <div>
-
-    </div>
-  )
+    <Container fluid={ true }>
+      <h4>Contact</h4>
+      <p>{ faker.lorem.paragraph() }</p>
+    </Container>
+  );
 }
 
 export default Contact;
