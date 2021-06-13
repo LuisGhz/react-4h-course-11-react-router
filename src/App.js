17ef45d35451dd6cce194535886661d95b1dbc25
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-13 13:30:33 -0500

    Add post_id route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from 'routes/Home';
 import About from 'routes/About';
 import Contact from 'routes/Contact';
+import Post from 'routes/Post';
 import MainNavBar from 'routes/MainNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Route exact path="/" component={ Home } />
       <Route path="/about" component={ About } />
       <Route path="/contact" component={ Contact } />
+      <Route path="/:post_id" component={ Post } />
     </Router>
   );
 }
