216c46ee1fe14b6ec0df3b37506854480900736e
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-13 13:39:14 -0500

    Fetch specific post from params.

--- src/routes/Post.js
+++ src/routes/Post.js
@@ -1,16 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
+import axios from 'axios';
 
 const Post = props => {
   console.log(props)
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${props.match.params.post_id}`)
+      setPost(res.data);
+    })()
+  }, [props.match.params.post_id])
+
   return (
     <div>
       <Card className="shadow p-3 mb-5 bg-white rounde">
-        <Card.Header>{ props.match.params.post_id }</Card.Header>
+        <Card.Header className="text-center" >{ post.title }</Card.Header>
         <Card.Body>
-          <Card.Title>
-            Title
-          </Card.Title>
-          <Card.Text>Body</Card.Text>
+          <Card.Text className="text-center" >{ post.body }</Card.Text>
         </Card.Body>
       </Card>
     </div>
