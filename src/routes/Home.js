b60101934145a3de586223d80f1472c041ec7828
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-13 13:19:27 -0500

    Get post from jsonplaceholder with axios Show posts with react-bootstrap cards

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,31 @@
-import { Container } from 'react-bootstrap';
-import faker from 'faker';
+import { useEffect, useState } from 'react';
+import { Container, Card } from 'react-bootstrap';
+import axios from 'axios';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      setPosts(res.data.slice(0, 5));
+    })()
+  }, [])
+
+  const listMarkup = posts.length > 0 ? (
+    posts.map(post => (
+        <Card key={ post.id } className="shadow p-3 mb-5 bg-white rounde">
+          <Card.Body>
+            <Card.Title>{post.title}</Card.Title>
+            <Card.Text>{post.body}</Card.Text>
+          </Card.Body>
+        </Card>
+    ))
+  ) : (<p>Not cards founds</p>);
+
   return (
-    <Container fluid={ true }>
+    <Container fluid={true}>
       <h4>Home</h4>
-      <p>{ faker.lorem.paragraph() }</p>
+      { listMarkup }
     </Container>
   );
 }
