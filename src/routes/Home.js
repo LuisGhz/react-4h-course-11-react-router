8d1a1287376f278e9cdf25cd2ea4a55680a45c99
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-13 13:30:49 -0500

    Set link to on title card

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Card } from 'react-bootstrap';
 import axios from 'axios';
 
@@ -15,7 +16,9 @@ const Home = () => {
     posts.map(post => (
         <Card key={ post.id } className="shadow p-3 mb-5 bg-white rounde">
           <Card.Body>
-            <Card.Title>{post.title}</Card.Title>
+            <Card.Title>
+              <Link to={ `/${post.id}` } >{ post.title }</Link>
+            </Card.Title>
             <Card.Text>{post.body}</Card.Text>
           </Card.Body>
         </Card>
