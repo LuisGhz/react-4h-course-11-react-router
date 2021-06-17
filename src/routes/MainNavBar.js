8fecce5273ba48615e3d277c88e8657f6650d4b5
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-17 18:10:05 -0500

    Create authHandler and set text to login button

--- src/routes/MainNavBar.js
+++ src/routes/MainNavBar.js
@@ -1,15 +1,32 @@
 import { useEffect } from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import { Link, withRouter } from 'react-router-dom';
+import auth from 'auth/Auth';
 
 const MainNavBar = props => {
-  console.log(props);
+  // console.log(props);
   // useEffect(() => {
   //   setTimeout(() => {
   //     props.history.push('/about');
   //   }, 2000);
   // // eslint-disable-next-line react-hooks/exhaustive-deps
   // }, []);
+
+  const authHandler = () => {
+    if (auth.isAuthenticated()) {
+      auth.logout(() => {
+        props.history.push('/');
+      });
+    }
+    if (!auth.isAuthenticated()) {
+      auth.login(() => {
+        props.history.push('/about');
+      });
+    }
+  }
+
+  const authText = auth.isAuthenticated() ? 'Logout' : 'Login';
+
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
@@ -20,7 +37,7 @@ const MainNavBar = props => {
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
           <Nav>
-            <Nav><Button variant="primary" >Login</Button></Nav>
+            <Nav><Button variant="primary" onClick={ authHandler } >{ authText }</Button></Nav>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
